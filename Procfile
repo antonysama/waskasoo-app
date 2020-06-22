675efe04d0f8247a67057b8b8b3f6a11f24ec800
antonysama <antonysama@gmail.com>
2020-06-22 17:04:07 -0600

    waskasoo-app deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run waskasoo_app.py
---
+++ waskasoo-app.py
@@ -0,0 +1,11 @@
+
+import streamlit as st 
+
+def main():
+	""" A simple waskasoo app """
+
+	st.title("waskasoo app with streamlit")
+	st.subheader("Streamlit is Cool")
+
+if __name__ == '__main__':
+	main()
